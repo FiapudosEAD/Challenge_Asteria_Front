a0c2be98ddee5b15bac10744b1cb98156c605f3a
Pedro Schmitz Sécio <pedro.secio014@gmail.com>
2025-10-31 14:07:03 -0300

    fix: import input

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import Input from "../components/Input";
+import Input from "../components/input";
 import Button from "../components/buttons";
 import { useAuth } from "../context/AuthContext"; 
 
